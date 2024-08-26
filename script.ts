eb6c8a0d32556c286677ff94f588018bdf4eb4a1
academus01i <md.ads01i@gmail.com>
2024-08-25 23:12:58 -0300

    Update script.ts

--- script.ts
+++ script.ts
@@ -10,7 +10,6 @@ form.addEventListener('submit', (event) => {
     const email = (document.getElementById('email') as HTMLInputElement).value;
 
     if (name && email) {
-        // Aqui, você poderia adicionar a lógica para enviar esses dados para um servidor
         message.textContent = Obrigado por se inscrever, ${name}!;
         form.reset();
     } else {
